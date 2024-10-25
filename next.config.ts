609308b2873ddb363b4e82944ebf0a4951cb766e
Ivan Kameniev <80778099+kamiv1980@users.noreply.github.com>
2024-10-25 18:55:33 +0200

    fixed config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export",
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/test-product-app/' : '',
-  basePath: isProd ? '/test-product-app' : '',
-  output: 'export'
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
